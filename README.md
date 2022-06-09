7568f379e54b7c83b86b202a762c3b4106cb57e3
Chris Jackson <chrisjackson@Chriss-MBP.fios-router.home>
2022-06-09 16:50:03 -0400

    weather demo final

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Weather-Service-API-Demo
+
+## Description
+
+Our challenge for this project was  to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+#### HTML
+
+    The syntax used in the HTML was:
+    
+    - Made from scratch.
+    - I had to set up alot of containers to be able make a dashboard, cards, and a container for the search city area.
+
+#### CSS
+
+    The syntax used in the CSS was
+
+    - Me simply just using boostrap.
+    - I didn't have to hardcode a css sheet.
+
+#### Javascript
+
+    - The javasctipt took a while for me to script out.
+    - Two functions were created, that carry out different tasks.
+
+    Screenshot
+
+    ![alt text](Weather-Service-API-Demo.jpeg)
+
+    Link to Deployed Application
+
+    
+
--- javascript.js
+++ javascript.js
@@ -10,19 +10,20 @@ var searchBtn = document.getElementById('user-form')
 
 var currentWeatherEL = document.getElementById('currentWeather');
 
+var city = ''
 
 
-function capValue(event){
+
+function capValue(event) {
     event.preventDefault()
-    var cityCapture = document.getElementById("citySearch")
-    
-    getForecast(cityCapture.value)
+    city = document.getElementById("citySearch").value
+    getForecast()
 }
 
 
-function getForecast(city) {
-   
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q='+ city +'&appid=' + apiKey)
+function getForecast() {
+
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey)
         .then(function (response) {
 
             return response.json()
@@ -32,51 +33,74 @@ function getForecast(city) {
 
             var lat = data[0].lat;
             var lon = data[0].lon;
-            var cityName = data[0].name
-            console.log(lat, lon);
 
             fetch('http://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + "&units=imperial")
                 .then(function (response) {
                     return response.json()
                 })
                 .then(function (weatherData) {
-                   getCurrentWeatherData(weatherData.current, cityName)
+                    console.log(weatherData)
+                    getCurrentWeatherData(weatherData.current)
                 })
         })
     //
 }
 
-function getCurrentWeatherData(weather, city){
-console.log(weather)
-var temp = weather.temp; 
-var tempEL = document.createElement("p");
-tempEL.textContent = temp
+function getCurrentWeatherData(weather) {
+    console.log(weather)
+    var temp = weather.temp;
+    var tempEL = document.createElement("p");
+    tempEL.textContent = 'Temperature: ' + temp + 'F';
     console.log(tempEL);
-//humidity
-var humidity = weather.humidity;
-var humidityEL = document.createElement("p");
-humidityEL.textContent = humidity
-//wind speed
-var speed = weather.wind_speed;
-var speedEL = document.createElement("p");
-speedEL.textContent = speed
-//uv index
-var uvi = weather.uvi;
-var uviEL = document.createElement("p");
-uviEL.textContent = uvi
+    //humidity
+    var humidity = weather.humidity;
+    var humidityEL = document.createElement("p");
+    humidityEL.textContent = 'Humidity: ' + humidity + '%';
+    //wind speed
+    var speed = weather.wind_speed;
+    var speedEL = document.createElement("p");
+    speedEL.textContent = 'Wind: ' + speed + 'mph';
+    //uv index
+    var uvi = weather.uvi;
+    var uviEL = document.createElement("p");
+    uviEL.textContent = 'UV index: ' + uvi;
+    if (uvi <= 3){
+        console.log('lowUvi')
+        uviEL.style.backgroundColor = 'lightblue';
+    } else if (uvi <= 6){
+        console.log('mediumUvi')
+        uviEL.style.backgroundColor = 'yellow';
+    } else {
+        console.log('highUvi')
+        uviEL.style.backgroundColor = 'orange';
+    }
 
-var icon = weather.weather[0].icon;
-var iconEL = document.createElement("img");
-iconEL.setAttribute("src", icon);
 
-var date = weather.dt;
-var dateEL = document.createElement("h1");
-dateEL.textContent = date 
-currentWeatherEL.append(tempEL, humidityEL, speedEL , uviEL, iconEL, dateEL);
+    var icon = weather.weather[0].icon;
+    var iconEL = document.createElement("img");
+    var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+    iconEL.setAttribute("src", iconurl);
+
+    var unix_timestamp = weather.dt;
+   
+        var date = new Date(unix_timestamp * 1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = date.getFullYear();
+        var month = months[date.getMonth()];
+        var date = date.getDate();
+        var time = month + ' ' + date + ' ' + year + ' ' ;
+  
+      
+
+console.log(time);
+    var dateEL = document.createElement("h1");
+    dateEL.textContent = city + ' ' + time;
+    currentWeatherEL.innerHTML = ''
+    currentWeatherEL.append(dateEL,iconEL, tempEL, humidityEL, speedEL, uviEL ) ;
 
 }
 
- //var fiveDays = 'http://api.openweathermap.org/data/2.5/forecast?lat&lon&appid=' + apiKey;
+//var fiveDays = 'http://api.openweathermap.org/data/2.5/forecast?lat&lon&appid=' + apiKey;
 
 
 
